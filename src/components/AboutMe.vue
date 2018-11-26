b2453e4eecfe4f5c095be11b6cf39cd761e90b34
Gabriel <gabriel@Kazaps-Mac-mini.local>
2018-11-26 13:23:51 -0200

    remove unused fonts from imports

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -2,8 +2,24 @@
   <div class="about-me">
     <h1>Gabriel Rigon</h1>
     <h2>About me</h2>
-    <p><strong>"I'm Batman!"</strong> I'm kidding hahah... or not!? <br><br> 
-    Currently working as <strong>Software Engineer and Technical Leader</strong> at <strong>Kazap Tecnologia</strong>. I love code, technology, pizza and rock and roll 🤘🏼</p>
+    <p>
+      <strong>"I'm <a
+        href="http://batman.com"
+        target="_blank"
+        tabindex="-1"
+        class="normal-text"
+      >Batman</a>!"</strong> hahaha, I'm kidding... or not!?
+      <br>
+      <br>Currently working as
+      <strong>Software Engineer and Technical Leader</strong> at
+      <strong>Kazap Tecnologia</strong>. I love code, technology, pizza and
+      <a
+        href="https://www.youtube.com/watch?v=TRqiFPpw2fY"
+        target="_blank"
+        tabindex="-1"
+        class="normal-text"
+      >rock and roll</a> 🤘🏼
+    </p>
   </div>
 </template>
 
@@ -17,5 +33,11 @@ export default {
 h1 {
   background-color: #bad5e2;
   color: white;
+}
+
+a.normal-text {
+  text-decoration: none;
+  cursor: text;
+  outline: none;
 }
 </style>
