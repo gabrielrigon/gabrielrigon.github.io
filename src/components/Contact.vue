c8a382e5e30331729f2a70177cbfc38d49c2b165
Gabriel Rigon <gabrielrigon@outlook.com>
2020-09-10 16:14:52 -0300

    update email address

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,8 +4,8 @@
     <p>
       For now, I only have an email to contact me, but in a few years I will buy a mobile phone, I promise.
       <a
-        href="mailto:devrigon@icloud.com"
-      >devrigon@icloud.com</a>
+        href="mailto:habla@gabrielrigon.com.br"
+      >habla@gabrielrigon.com.br</a>
     </p>
   </div>
 </template>
